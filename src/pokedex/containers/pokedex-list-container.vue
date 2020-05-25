644bc385cf83af283a363d1d895ce55cd1d82405
Víctor Martínez <victorandresmtz@gmail.com>
2020-05-24 19:55:54 -0500

    feat(app): add Bootstrap

---
+++ src/pokedex/containers/pokedex-list-container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    {{ list }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokedexListContainer',
+  data() {
+    return {
+      list: [{ name: 'Bulbasaur' }]
+    };
+  }
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import Home from '@/views/Home.vue';
+import PokedexListContainer from '@/pokedex/containers/pokedex-list-container.vue';
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 
@@ -7,17 +7,8 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Pokedex',
+    component: PokedexListContainer
   }
 ];
 
