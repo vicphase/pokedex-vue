043b2b7418e08e074a41fab08b08454d81dc41a4
Víctor Martínez <victorandresmtz@gmail.com>
2020-05-24 19:36:24 -0500

    feat(app): change linting rules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld
   }
